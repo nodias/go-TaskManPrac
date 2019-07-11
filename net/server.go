8f6976c889b61646a0fb93c4fc2754ebf20d24b3
kss <kss@antlinesoft.com>
2019-07-11 18:07:34 +0900

    add handler

--- net/server.go
+++ net/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -25,10 +26,37 @@ func myhandler(w http.ResponseWriter, r *http.Request) {
 		return id, nil
 	}
 
-	getTasks := func() []task.Task {
-
-		return nil
+	getTasks := func() ([]task.Task, error) {
+		var result []task.Task
+		err := r.ParseForm()
+		if err!= nil{
+			return nil, err
+		}
+		encodedTasks, ok := r.PostForm["task"]
+		if !ok {
+			return nil, errors.New("task parameter expected")
+		}
+		for _, encodedTask := range encodedTasks{
+			var t task.Task
+			if err:= json.Unmarshal([]byte(encodedTask), &t); err != nil {
+				return nil, err
+			}
+			result = append(result, t)
+		}
+		return result, nil
 	}
+
+	switch r.Method {
+	case "GET":
+		panic("not implement")
+	case "POST":
+		panic("not implement")
+	case "PUT":
+		panic("not implement")
+	case "DELETE":
+		panic("not implement")
+	}
+
 	fmt.Println(getID())
 	fmt.Println(getTasks())
 
